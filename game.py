139bd641e560c4a8eff518e0b6b801e88d1ba074
JCHubbard659 <jchubbard2010@gmail.com>
2026-09-15 20:22:44 -0500

    Build Hero Class

--- game.py
+++ game.py
@@ -1,22 +1,34 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "Tokyo, Japan"
 
+def battle(hero: Hero, enemy: Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        hero_damage = hero.attack()
+        enemy.take_damage(hero_damage)
+
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
+
+    if hero.is_alive():
+        print(f"{hero.name} wins!")
+    else:
+        print(f"{enemy.name} wins!")
 
 def main():
-    """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-goblin = Goblin("Naruto")
-goblin2 = Goblin("Batman")
+    goblin = Goblin("Batman")
+    print(f"{goblin.name} enters the arena with {goblin.health} health.")
 
-print(f"{goblin.name} enters the arena with {goblin.health} health.")
-print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
-print("But no hero has answered the call... yet.")
+    bob = Hero("Batman")
+    print(f"{bob.name} the {bob.hero_class} enters the arena with {bob.health} health.")
 
+    battle(bob, goblin)
 
 if __name__ == "__main__":
     main()
